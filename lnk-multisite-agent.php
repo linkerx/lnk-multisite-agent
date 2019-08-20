9c7d16915f8b4fa1fef0f2ae6a7898951f69e937
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-08-20 16:05:41 -0300

    featured posts

--- lnk-multisite-agent.php
+++ lnk-multisite-agent.php
@@ -12,8 +12,9 @@
 
 function lnk_multisite_agent_metadata($id, $post) {
 
-    update_post_meta($post->ID, 'lnk_revision', 0);
+    update_post_meta($post->ID, 'lnk_checked', 0);
     update_post_meta($post->ID, 'lnk_onhome', 0);
+    update_post_meta($post->ID, 'lnk_socialpost',0);
 
 }
 add_action('publish_post', 'lnk_multisite_agent_metadata', 10, 2 );
