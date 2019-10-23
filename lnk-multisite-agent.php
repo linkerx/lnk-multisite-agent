19287e6ed44e8fa1a2e52a15895087bae679acdb
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-10-23 15:20:39 -0300

    social

--- lnk-multisite-agent.php
+++ lnk-multisite-agent.php
@@ -12,9 +12,11 @@
 
 function lnk_multisite_agent_metadata($id, $post) {
 
-    update_post_meta($post->ID, 'lnk_checked', 0);
-    update_post_meta($post->ID, 'lnk_onhome', 0);
-    update_post_meta($post->ID, 'lnk_socialpost',0);
+    update_post_meta($post->ID, 'lnk_checked', 0); //revisado por manager
+    update_post_meta($post->ID, 'lnk_onhome', 0); //publicado en home
+    update_post_meta($post->ID, 'lnk_featured', 0); //destacado
+    update_post_meta($post->ID, 'lnk_featured_mode', 0); //modo de impresion de destacado
+    update_post_meta($post->ID, 'lnk_socialpost',0); //publicacion en redes sociales
 
 }
 add_action('publish_post', 'lnk_multisite_agent_metadata', 10, 2 );
