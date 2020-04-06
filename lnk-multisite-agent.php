430f30d450f6c779c8fab7d2c1b8b833909dad4d
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2020-04-06 12:10:18 -0300

    solo en crecion

--- lnk-multisite-agent.php
+++ lnk-multisite-agent.php
@@ -12,13 +12,15 @@
 
 function lnk_multisite_agent_metadata($id, $post) {
 
-    update_post_meta($post->ID, 'lnk_checked', 0); //revisado por manager
-    update_post_meta($post->ID, 'lnk_onhome', 0); //publicado en home
-    update_post_meta($post->ID, 'lnk_featured', 0); //destacado
-    update_post_meta($post->ID, 'lnk_featured_mode', 0); //modo de impresion de destacado
-    update_post_meta($post->ID, 'lnk_onagenda',0); //publicacion en agenda
-    update_post_meta($post->ID, 'lnk_agenda',0); //fecha de publicacion en agenda
-    update_post_meta($post->ID, 'lnk_facebook_count',0); //publicacion en redes sociales
-    update_post_meta($post->ID, 'lnk_site_metadata',""); //otros datos propios del sitio
+    if( !wp_is_post_revision( $post->ID ) ) {
+        update_post_meta( $post->ID, 'lnk_checked', 0 ); //revisado por manager
+        update_post_meta( $post->ID, 'lnk_onhome', 0 ); //publicado en home
+        update_post_meta( $post->ID, 'lnk_featured', 0 ); //destacado
+        update_post_meta( $post->ID, 'lnk_featured_mode', 0 ); //modo de impresion de destacado
+        update_post_meta( $post->ID, 'lnk_onagenda',0 ); //publicacion en agenda
+        update_post_meta( $post->ID, 'lnk_agenda',0 ); //fecha de publicacion en agenda
+        update_post_meta( $post->ID, 'lnk_facebook_count',0 ); //publicacion en redes sociales
+        update_post_meta( $post->ID, 'lnk_site_metadata',"" ); //otros datos propios del sitio
+    }
 }
 add_action('publish_post', 'lnk_multisite_agent_metadata', 10, 2 );
